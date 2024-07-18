8f4a86f284d69f2d1bb7a097c6b7bee672e5ef4f
Francesco Poli <polifr@gmail.com>
2024-07-19 00:10:55 +0200

    Inizializzazione progetto

---
+++ src/main/java/it/poli/csrf_servlet/CsrfServletApplication.java
@@ -0,0 +1,12 @@
+package it.poli.csrf_servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsrfServletApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CsrfServletApplication.class, args);
+  }
+}
---
+++ src/test/java/it/poli/csrf_servlet/CsrfServletApplicationTests.java
@@ -0,0 +1,11 @@
+package it.poli.csrf_servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsrfServletApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
